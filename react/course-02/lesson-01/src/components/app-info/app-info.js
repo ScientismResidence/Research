a4cd3496819ebb1ae0a160b8c96f0454b9405236
Dmitrii Urusov <Dmitrii.Urusov@dataart.com>
2022-07-29 22:13:13 +0400

    Created structure of first app

---
+++ react/course-02/lesson-01/src/components/app-info/app-info.js
@@ -0,0 +1,13 @@
+import './app-info.css';
+
+const AppInfo = () => {
+    return (
+        <div className="app-info">
+            <h1>Employees in company</h1>
+            <h2>Total employees: </h2>
+            <h2>Total premiumed employees: </h2>
+        </div>
+    );
+}
+
+export default AppInfo;
